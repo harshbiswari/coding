4a273f1e208a03afa5a18f8dac367ba7eeb9a60b
harshbiswari <harshbiswari88@gmail.com>
2024-05-24 22:20:54 +0530

    working code

--- C programming/Threads/thread_example1.c
+++ C programming/Threads/thread_example1.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
+#include<unistd.h>
+
+void* thread_fun(void *arg) {
+    printf("Hello from thread\n");
+    return NULL;
+}
 
 int main() {
-    
+    pthread_t tid;
+    pthread_create(&tid, NULL, &thread_fun, NULL);
+    sleep(1);
+    pthread_join(tid, NULL);
+    return 0;
 }
