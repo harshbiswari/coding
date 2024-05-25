4bb05f86e129eebc49a936312245db4edee88365
harshbiswari <harshbiswari88@gmail.com>
2024-05-25 11:18:47 +0530

    singal testing

--- c_programming/signals/signal_example.c
+++ c_programming/signals/signal_example.c
@@ -8,10 +8,12 @@ void handle_sigtstp(int sig) {
 }
 
 int main(int argc, char* argv[]) {
+    //Harsh Testing
     struct sigaction sa;
     sa.sa_handler = &handle_sigtstp;
     sa.sa_flags = SA_RESTART;
     sigaction(SIGINT, &sa, NULL);
+    printf("This is signal testing code\n");
     while(1);		
 	
     return 0;
