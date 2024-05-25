038a191455fc3ea5667ebbfd25c4804903c68ef5
hbiswari <harshbiswari88@gmail.com>
2024-05-25 10:52:05 +0530

    signal change

--- c_programming/signals/signal_example.c
+++ c_programming/signals/signal_example.c
@@ -1,19 +1,18 @@
-#define  _POSIX_C_SOURCE  200809L
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 
 
 void handle_sigtstp(int sig) {
-    printf("Stopped not allowed, Caught signal %d\n", sig);
+    printf("Interrupt not allowed, Caught signal %d\n", sig);
 }
 
 int main(int argc, char* argv[]) {
     struct sigaction sa;
     sa.sa_handler = &handle_sigtstp;
     sa.sa_flags = SA_RESTART;
-    sigaction(SIGTSTP, &sa, NULL);
-
+    sigaction(SIGINT, &sa, NULL);
+    while(1);		
+	
     return 0;
 }
